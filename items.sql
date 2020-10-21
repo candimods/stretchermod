8fa18547db820ed74f7f596035e86309d35f5d41
candimods <73163398+candimods@users.noreply.github.com>
2020-10-21 13:23:11 -0400

    Update items.sql
    upded to add more stretchers

--- items.sql
+++ items.sql
@@ -1,5 +1,3 @@
-INSERT INTO `items`   -- {hash = `name of vehicle`, item = 'es_extend item name', remove = how many item to remove on use},
-    {hash = `Stretcher`,          item = 'stretcher',  remove = 1}, -- When using the stretcher  item, it will spawn a Stretcher vehicle and remove 1x item
-    {hash = `stryker_M1`,         item = 'stretcher2', remove = 1}, -- When using the stretcher2 item, it will spawn a stryker_M1 vehicle and remove 1x item
-    {hash = `stryker_M1_coroner`, item = 'stretcher3', remove = 1}, -- When using the stretcher3 item, it will spawn a stryker_M1_coroner vehicle and remove 1x item
-}
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('stretcher', 'Stretcher', 2, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('stretcher2', 'Stretcher2', 2, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('stretcher3', 'Stretcher3', 2, 0, 1);
