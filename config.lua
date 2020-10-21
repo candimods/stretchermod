1d1b41b714d780cbb8189a5d9cd5cc0d8d982c3f
candimods <73163398+candimods@users.noreply.github.com>
2020-10-21 13:25:55 -0400

    Update config.lua
    updates to add more stretchers

--- config.lua
+++ config.lua
@@ -25,7 +25,9 @@ Config.Hash = {
 
 Config.ItemsVeh = {
   -- {hash = `name of vehicle`, item = 'es_extend item name', remove = how many item to remove on use},
-	{hash = `Stretcher`, item = 'stretcher', remove = 0}, -- When using the stretcher item, it will spawn a Stretcher vehicle and remove 0x item
+    {hash = `Stretcher`,          item = 'stretcher',  remove = 1}, -- When using the stretcher  item, it will spawn a Stretcher vehicle and remove 1x item
+    {hash = `stryker_M1`,         item = 'stretcher2', remove = 1}, -- When using the stretcher2 item, it will spawn a stryker_M1 vehicle and remove 1x item
+    {hash = `stryker_M1_coroner`, item = 'stretcher3', remove = 1}, -- When using the stretcher3 item, it will spawn a stryker_M1_coroner vehicle and remove 1x 
 }
 
 Config.Press = {
